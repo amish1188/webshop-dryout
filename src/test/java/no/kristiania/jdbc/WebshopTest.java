3fd3748e8eadc5d69f7572fbf92c89a0112b0bed
amish1188 <martin.joodd@gmail.com>
2019-10-09 15:46:11 +0200

    Add PostrgeSQL support

--- src/test/java/no/kristiania/jdbc/WebshopTest.java
+++ src/test/java/no/kristiania/jdbc/WebshopTest.java
@@ -1,15 +1,22 @@
 package no.kristiania.jdbc;
 
+import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
 import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WebshopTest {
     @Test
-    void shouldRetrieveStoreProduct(){
-        ProductDao dao = new ProductDao();
+    void shouldRetrieveStoreProduct() throws SQLException {
+        JdbcDataSource dataSource = new JdbcDataSource();
+        dataSource.setUrl("jdbc:h2:mem:test");
+
+        dataSource.getConnection().createStatement().executeUpdate("create table products (name varchar(100))");
+
+        ProductDao dao = new ProductDao(dataSource);
         String productName = pickOne(new String[] {"Apples", "Bananas", "Wallnuts", "Grapes"});
         dao.insertProduct(productName);
         assertThat(dao.listAll()).contains(productName);
